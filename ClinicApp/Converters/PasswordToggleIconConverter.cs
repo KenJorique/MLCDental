c9ec77696cd664226d21b0a3b97d3328c6bca58d
LAPTOP-TRKKH7PT\Leah Marie Bolasco <leahmariebolasco@gmail.com>
2026-09-19 15:39:14 +0800

    fx

--- ClinicApp/Converters/PasswordToggleIconConverter.cs
+++ ClinicApp/Converters/PasswordToggleIconConverter.cs
@@ -8,7 +8,7 @@ public class PasswordToggleIconConverter : IValueConverter
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         bool isHidden = value is bool b && b;
-        return isHidden ? "\ue8f5" : "\ue8f4";
+        return isHidden ? "\ue8f4" : "\ue8f5";
     }
 
     // One-way binding only; reverse conversion isn't needed for an icon glyph.
